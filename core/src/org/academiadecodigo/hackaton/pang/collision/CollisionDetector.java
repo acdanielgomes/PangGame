f1e03e94a04d2775af5659bf3e86e4f57219a443
Daniel Gomes <daniel.af.gomes@aol.com>
2016-07-07 17:48:05 +0100

    first commit, initial skeleton

---
+++ core/src/org/academiadecodigo/hackaton/pang/collision/CollisionDetector.java
@@ -0,0 +1,31 @@
+package org.academiadecodigo.hackaton.pang.collision;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+/**
+ * Created by codecadet on 07/07/16.
+ */
+public class CollisionDetector implements ContactListener {
+    @Override
+    public void beginContact(Contact contact) {
+
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+}
---
+++ core/src/org/academiadecodigo/hackaton/pang/sprites/Ball.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.hackaton.pang.sprites;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Created by codecadet on 07/07/16.
+ */
+public class Ball extends Sprite {
+}
