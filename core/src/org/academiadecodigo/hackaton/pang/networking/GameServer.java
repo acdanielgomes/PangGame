325bbadc6dedd226ff9c9c9e513b8e5a18c72e08
Andre Duarte <duarteandrem@gmail.com>
2016-07-08 10:00:50 +0100

    Initial branch commit

---
+++ core/src/org/academiadecodigo/hackaton/pang/networking/GameServer.java
@@ -0,0 +1,32 @@
+package org.academiadecodigo.hackaton.pang.networking;
+
+import java.net.DatagramPacket;
+
+/**
+ * Created by codecadet on 08/07/16.
+ */
+public class GameServer implements Runnable {
+
+    private DatagramPacket serverSocket;
+
+
+
+    @Override
+    /**
+     * @see Runnable#run()
+     */
+    public void run() {
+
+    }
+
+    private class ClientThread implements Runnable {
+
+        @Override
+        /**
+         * @see Runnable#run()
+         */
+        public void run() {
+
+        }
+    }
+}
