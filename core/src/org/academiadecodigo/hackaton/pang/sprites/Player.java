38cb656e02926d795c14c192fb7bd82c7d826b3e
Daniel Gomes <daniel.af.gomes@aol.com>
2016-07-08 01:34:53 +0100

    comments on player class

--- core/src/org/academiadecodigo/hackaton/pang/sprites/Player.java
+++ core/src/org/academiadecodigo/hackaton/pang/sprites/Player.java
@@ -2,10 +2,8 @@ package org.academiadecodigo.hackaton.pang.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-import com.sun.xml.internal.ws.encoding.fastinfoset.FastInfosetCodec;
 import org.academiadecodigo.hackaton.pang.PangGame;
 import org.academiadecodigo.hackaton.pang.screens.PlayScreen;
 
@@ -14,15 +12,23 @@ import org.academiadecodigo.hackaton.pang.screens.PlayScreen;
  */
 public class Player extends Sprite {
 
+    /* Manages all physics GameObjects */
     public World world;
 
-    public Body getBody() {
-        return b2Body;
-    }
-
+    /* Receive impulses and forces */
     private Body b2Body;
+
+    /* Describes properties (size and shape) of an object */
     public Fixture fixture;
 
+    /**
+     * Constructor of the Player
+     * Set texture, size and reference to the game world
+     *
+     * @param screen Game references
+     * @param x Position X axis
+     * @param y Position Y axis
+     */
     public Player(PlayScreen screen, float x, float y) {
 
         super(new Texture("badlogic.jpg")); // TODO: 07/07/16 player image
@@ -30,14 +36,30 @@ public class Player extends Sprite {
         this.setSize(PangGame.PLAYER_WIDTH / PangGame.PPM, PangGame.PLAYER_HEIGHT / PangGame.PPM);
 
         definePlayer(x, y);
-
     }
 
+    /**
+     * Update properties of the Player
+     *
+     * @param dt Time since the last update
+     */
     public void update(float dt) {
 
         setPosition(b2Body.getPosition().x - getWidth() / 2, b2Body.getPosition().y - getHeight() / 2);
     }
 
+    /**
+     * Define the Player characteristics
+     *
+     * Creates a body with a set of definitions
+     * Set the position and the body type
+     *
+     * Sets the fixture (size and shape)
+     * Defines the material properties
+     *
+     * @param x
+     * @param y
+     */
     public void definePlayer(float x, float y) {
 
         BodyDef bDef = new BodyDef();
@@ -68,6 +90,15 @@ public class Player extends Sprite {
         fixture = b2Body.createFixture(fixtureDef);
         fixture.setUserData(this);
 
+    }
+
+    /**
+     * Getter for Body
+     *
+     * @return body
+     */
+    public Body getBody() {
+        return b2Body;
     }
 
 }
