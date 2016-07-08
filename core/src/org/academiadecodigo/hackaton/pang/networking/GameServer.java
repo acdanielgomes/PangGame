7c3f9c482c7f49125c9f8971aae304f19d273bc3
Andre Duarte <duarteandrem@gmail.com>
2016-07-08 10:09:23 +0100

    Stuff

--- core/src/org/academiadecodigo/hackaton/pang/networking/GameServer.java
+++ core/src/org/academiadecodigo/hackaton/pang/networking/GameServer.java
@@ -1,14 +1,15 @@
 package org.academiadecodigo.hackaton.pang.networking;
 
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
 /**
  * Created by codecadet on 08/07/16.
  */
 public class GameServer implements Runnable {
 
-    private DatagramPacket serverSocket;
-
+    private DatagramSocket serverSocket;
 
 
     @Override
@@ -16,6 +17,23 @@ public class GameServer implements Runnable {
      * @see Runnable#run()
      */
     public void run() {
+
+        try {
+
+            serverSocket = new DatagramSocket(8080);
+            byte[] message = new byte[5];
+
+            while (true) {
+//                DatagramPacket receivePacket = new DatagramPacket(data, data.length);
+//
+//               serverSocket.receive(message);
+
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
